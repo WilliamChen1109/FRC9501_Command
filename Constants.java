11a0aa6879fe0b992f9b5b0d2617d0463fd20e71
Chen, Wei-Yu <49446811+WilliamChen1109@users.noreply.github.com>
2024-01-04 20:10:46 +0800

    Create Constants.java

---
+++ Constants.java
@@ -0,0 +1,16 @@
+package frc.robot;
+
+public final class Constants {
+  public static class OperatorConstants {
+    public static final int kDriverControllerPort = 0;
+  }
+
+  public static class ChassisConstants{
+    public static final int kLeft1MotorID = 0;
+    public static final int kLeft2MotorID = 1;
+    public static final int kRight1MotorID = 2;
+    public static final int kRight2MotorID = 3;
+
+    public static final double kDrivingSpeed = 0.6;
+  }
+}
