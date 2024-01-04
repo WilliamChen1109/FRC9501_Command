6dfdd99dd9ee21ae132486f260c91bdf60155d7d
Chen, Wei-Yu <49446811+WilliamChen1109@users.noreply.github.com>
2024-01-04 20:11:27 +0800

    Create RobotContainer.java

---
+++ RobotContainer.java
@@ -0,0 +1,37 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.XboxController.Button;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import frc.robot.commands.ChassisDriveCommand;
+import frc.robot.subsystems.Chassis;
+
+public class RobotContainer {
+  // 搖桿物件建立
+  private final XboxController mController = new XboxController(Constants.OperatorConstants.kDriverControllerPort);
+  // Subsystem宣告和new
+  private final Chassis mChassis = new Chassis();
+
+  // 建構式
+  public RobotContainer() {
+    configureBindings();
+  }
+
+  // 按鍵設定
+  private void configureBindings() {
+    // 按著就執行
+    new JoystickButton(mController, Button.kA.value).whileTrue(new ChassisDriveCommand(mChassis, 0.4, 0));
+    // 沒有按著就執行
+    new JoystickButton(mController, Button.kB.value).whileFalse(null);
+    // 按下的瞬間執行
+    new JoystickButton(mController, Button.kLeftBumper.value).onTrue(null);
+    // 放開的瞬間執行
+    new JoystickButton(mController, Button.kStart.value).onFalse(null);
+  }
+
+  // 傳自走程式碼用的
+  public Command getAutonomousCommand() {
+    return null;
+  }
+}
